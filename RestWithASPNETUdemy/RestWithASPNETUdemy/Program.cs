fa89919b3bc506015a8be2faa2ed91a8e708e7ff
Geovane-DEV20 <geovanedonizete12@gmail.com>
2023-08-02 19:33:42 -0300

    Ajuste repositorio

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Program.cs
@@ -0,0 +1,32 @@
+using RestWithASPNETUdemy.Services;
+using RestWithASPNETUdemy.Services.Implementations;
+
+namespace RestWithASPNETUdemy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            //Dependency Injection
+            builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/IPersonService.cs
@@ -0,0 +1,15 @@
+﻿using RestWithASPNETUdemy.Model;
+using System.Security.Cryptography;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        List<Person> FindAll();
+        Person FindById(long id);
+        
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
