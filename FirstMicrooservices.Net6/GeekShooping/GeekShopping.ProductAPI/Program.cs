7c89cb53f8e4d7a391c5fc86512c496661657ec6
Geovane-DEV20 <geovanedonizete12@gmail.com>
2023-08-03 20:00:17 -0300

    Meu primeiro microoservico

---
+++ FirstMicrooservices.Net6/GeekShooping/GeekShopping.ProductAPI/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.OpenApi.Models;
+
+namespace GeekShopping.ProductAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.ProductAPI", Version = "v1" });
+            });
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
